457cdefa5133874f11009eb566e9dcf683a86301
qt670717885 <670717885@qq.com>
2018-10-24 21:10:47 +0800

    '前端路由完成'

--- front-end/src/javascripts/app.js
+++ front-end/src/javascripts/app.js
@@ -1,10 +1,13 @@
-
+//引入样式
 import '../stylesheets/app.scss'
+//引入路由
+import router from './router'
+
 
 const body_template = require('./view/body.html')
-const cinemaList_template = require('./view/cinemaList.html')
 
 //渲染页面
 $('#wrapper').html(body_template);
-$('#content-view').html(cinemaList_template);
+
+router.init(); 
 
