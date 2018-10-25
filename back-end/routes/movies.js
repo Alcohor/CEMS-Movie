412c8d58e825e4c3d098e811e27b88d25ff92d79
Alcohor <1013674928@qq.com>
2018-10-25 17:36:08 +0800

    1

--- back-end/routes/movies.js
+++ back-end/routes/movies.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const movies = require('../controller/movies_controller')
+const fieupload = require('../middleware/moviesPicUpload')
 //routes/movies/get
 router.get('/get',movies.get)
-router.post('/save',movies.save)
+router.post('/save',fieupload,movies.save)
 router.get('/getbyid',movies.getById)
 router.get('/deletebyid',movies.delById)
 router.get('/getbyname',movies.getByName)
