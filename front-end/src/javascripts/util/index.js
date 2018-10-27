16047660adcc62d76f30875cc0aecc58bb1726c2
Alcohor <1013674928@qq.com>
2018-10-27 20:39:16 +0800

    11111

--- front-end/src/javascripts/util/index.js
+++ front-end/src/javascripts/util/index.js
@@ -1,13 +1,22 @@
 import getPath from './getPath'
 import bus from './bus'
-
+import toast from './toast'
+import handleToastByData from './handleToastByData'
 
 export {
     getPath,
-    bus
+    bus,
+    toast,
+    handleToastByData
 }
 
 export default {
     getPath,
-    bus
+    bus,
+    toast,
+    handleToastByData
 }
+
+
+
+
